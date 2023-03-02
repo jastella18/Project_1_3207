7fdce914f143716d5c878b357c2e257d3cdebb03
Joe Stella <jastella18@gmail.com>
2023-03-02 11:51:50 -0500

    added options to ps

--- ps.c
+++ ps.c
@@ -1,17 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
-    char buffer[1024];
-    FILE* pipe = popen("ps aux", "r");
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    char command[BUFFER_SIZE] = "ps -u $USER";
+    char buffer[BUFFER_SIZE];
+    FILE *pipe;
+
+    // Process -p option
+    if (argc >= 3 && strcmp(argv[1], "-p") == 0) {
+        strcat(command, " -p ");
+        strcat(command, argv[2]);
+    }
+
+    // Process -s option
+    if (argc >= 2 && strcmp(argv[argc - 1], "-s") == 0) {
+        strcat(command, " -o stat");
+    }
+
+    // Process -U option
+    if (argc >= 2 && strcmp(argv[argc - 1], "-U") != 0) {
+        strcat(command, " -o time");
+    }
+
+    // Process -S option
+    if (argc >= 2 && strcmp(argv[argc - 1], "-S") == 0) {
+        strcat(command, " -o stime");
+    }
+
+    // Process -v option
+    if (argc >= 2 && strcmp(argv[argc - 1], "-v") == 0) {
+        strcat(command, " -o vsz");
+    }
+
+    // Process -c option
+    if (argc >= 2 && strcmp(argv[argc - 1], "-c") != 0) {
+        strcat(command, " -o cmd");
+    }
+
+    // Open pipe and execute command
+    pipe = popen(command, "r");
     if (!pipe) {
         perror("Error opening pipe");
         return 1;
     }
+
+    // Read output from command and print to console
     printf("Process List:\n");
-    while (fgets(buffer, sizeof(buffer), pipe)) {
+    while (fgets(buffer, BUFFER_SIZE, pipe)) {
         printf("%s", buffer);
     }
+
+    // Close pipe and return
     pclose(pipe);
     return 0;
 }
