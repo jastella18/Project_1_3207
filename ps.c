c535bacf022b137e8d75a10c99c64ffce1fc2a59
Joe Stella <jastella18@gmail.com>
2023-03-02 12:21:34 -0500

    realized i was submitting old 'ps aux' version

--- ps.c
+++ ps.c
@@ -2,58 +2,105 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER_SIZE 1024
-
 int main(int argc, char *argv[]) {
-    char command[BUFFER_SIZE] = "ps -u $USER";
-    char buffer[BUFFER_SIZE];
-    FILE *pipe;
+    char path[100];
+    FILE *fp;
+    char line[1000];
 
-    // Process -p option
-    if (argc >= 3 && strcmp(argv[1], "-p") == 0) {
-        strcat(command, " -p ");
-        strcat(command, argv[2]);
+    // Default values of arguments
+    int pid = -1; // Display information for all processes
+    int show_state = 0;
+    int show_utime = 1;
+    int show_stime = 0;
+    int show_vmem = 0;
+    int show_cmdline = 1;
+
+    // Parse arguments
+    for (int i = 1; i < argc; i++) {
+        switch (argv[i][1]) {
+            case 'p':
+                pid = atoi(argv[++i]);
+                break;
+            case 's':
+                show_state = 1;
+                break;
+            case 'U':
+                show_utime = 0;
+                break;
+            case 'S':
+                show_stime = 1;
+                break;
+            case 'v':
+                show_vmem = 1;
+                break;
+            case 'c':
+                show_cmdline = 0;
+                break;
+            default:
+                printf("Unknown option: %s\n", argv[i]);
+                return 1;
+        }
     }
 
-    // Process -s option
-    if (argc >= 2 && strcmp(argv[argc - 1], "-s") == 0) {
-        strcat(command, " -o stat");
+    printf("%-8s %-8s %-8s", "PID", "STATE", "CMDLINE");
+    if (show_utime) {
+        printf(" %-8s", "UTIME");
     }
-
-    // Process -U option
-    if (argc >= 2 && strcmp(argv[argc - 1], "-U") != 0) {
-        strcat(command, " -o time");
+    if (show_stime) {
+        printf(" %-8s", "STIME");
     }
-
-    // Process -S option
-    if (argc >= 2 && strcmp(argv[argc - 1], "-S") == 0) {
-        strcat(command, " -o stime");
+    if (show_vmem) {
+        printf(" %-8s", "VMEM");
     }
+    printf("\n");
 
-    // Process -v option
-    if (argc >= 2 && strcmp(argv[argc - 1], "-v") == 0) {
-        strcat(command, " -o vsz");
-    }
-
-    // Process -c option
-    if (argc >= 2 && strcmp(argv[argc - 1], "-c") != 0) {
-        strcat(command, " -o cmd");
-    }
-
-    // Open pipe and execute command
-    pipe = popen(command, "r");
-    if (!pipe) {
-        perror("Error opening pipe");
+    // Open directory /proc and read its contents
+    if ((fp = fopen("/proc", "r")) == NULL) {
+        printf("Error: cannot open /proc\n");
         return 1;
     }
-
-    // Read output from command and print to console
-    printf("Process List:\n");
-    while (fgets(buffer, BUFFER_SIZE, pipe)) {
-        printf("%s", buffer);
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        // Check if the directory entry is a process ID
+        if (sscanf(line, "%d", &pid) == 1) {
+            sprintf(path, "/proc/%d/stat", pid);
+            fp = fopen(path, "r");
+            if (fp != NULL) {
+                int pid_;
+                char state;
+                char cmdline[1000];
+                unsigned long utime;
+                unsigned long stime;
+                unsigned long vmem;
+                fscanf(fp, "%d %s %c", &pid_, cmdline, &state);
+                if (pid == -1 || pid_ == pid) {
+                    printf("%-8d %-8c ", pid_, state);
+                    if (show_cmdline) {
+                        printf("%-8s", cmdline);
+                    }
+                    if (show_utime) {
+                        fscanf(fp, "%lu", &utime);
+                        printf(" %-8lu", utime);
+                    }
+                    if (show_stime) {
+                        fscanf(fp, "%lu", &stime);
+                        printf(" %-8lu", stime);
+                    }
+                    if (show_vmem) {
+                        sprintf(path, "/proc/%d/statm", pid_);
+                        fp = fopen(path, "r");
+                        if (fp != NULL) {
+                            fscanf(fp, "%lu", &vmem);
+                            printf(" %-8lu", vmem);
+                            fclose(fp);
+                        }
+                    }
+                    printf("\n");
+                }
+                fclose(fp);
+            }
+        }
     }
+    fclose(fp);
 
-    // Close pipe and return
-    pclose(pipe);
     return 0;
 }
